d4087eeb000657a9b1094495ea49000685e6e0dd
Chloé Pochon <74970056+chloepochon@users.noreply.github.com>
2023-06-11 16:08:56 +0200

    Update main6.js

--- threeJS/planetarium/solar-system/js/main6.js
+++ threeJS/planetarium/solar-system/js/main6.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from '/three';
 import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls';
 import { MeshStandardMaterial } from '/three/src/materials/MeshStandardMaterial';
 import { AmbientLight } from '/three/src/lights/AmbientLight';
