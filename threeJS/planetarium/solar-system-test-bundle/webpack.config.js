0692643874e02ce7beb7ea12d2b8316e286ee230
Chloé Pochon <74970056+chloepochon@users.noreply.github.com>
2023-06-16 23:33:00 +0200

    Add files via upload

---
+++ threeJS/planetarium/solar-system-test-bundle/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main6.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  }
+  // Autres règles et plugins Webpack...
+};
