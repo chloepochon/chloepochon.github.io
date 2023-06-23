7c0f664b927ae986a508f648a0fe4123e04230b9
Chloé Pochon <74970056+chloepochon@users.noreply.github.com>
2023-06-23 10:20:24 +0200

    Update main.js

--- threeJS/planetarium/solar-system/js/main.js
+++ threeJS/planetarium/solar-system/js/main.js
@@ -12,8 +12,9 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
 // Ajustement de la position de la caméra
-camera.position.set(230, 100, -50); // Décalage vers la droite, vers le haut et vers l'arrière
-camera.lookAt(0, 0, 0); // Focalisation sur le point central de la scène
+camera.position.set(230, 100, -50);
+// Focalisation sur le point central de la scène
+camera.lookAt(0, 0, 0); 
 
 // Ajustement du zoom de la caméra
 camera.zoom = 0.5; // Valeur de zoom plus petite que 1 pour un effet de dézoom
@@ -132,7 +133,7 @@ const elementMaterial = new THREE.MeshBasicMaterial({
   color: 0xFFFFFF, // Couleur de base de l'élément (blanc)
   side: THREE.DoubleSide, // Afficher les deux côtés de l'élément
   transparent: true, // Permettre la transparence
-  opacity: 0.5 // Opacité de l'élément (réglable selon vos préférences)
+  opacity: 0.5 // Opacité de l'élément
 });
 
 // Créer un objet Mesh pour l'élément
@@ -142,9 +143,8 @@ const elementMesh = new THREE.Mesh(elementGeometry, elementMaterial);
 const colors = [];
 
 for (let i = 0; i < elementGeometry.attributes.position.count; i++) {
-  const ratio = (elementGeometry.attributes.position.getY(i) - elementGeometry.parameters.innerRadius) /
+  const ratio = (elementGeometry.attributes.position.getY(i) - elementGeometry.parameters.innerRadius)
                 (elementGeometry.parameters.outerRadius - elementGeometry.parameters.innerRadius);
-  //const color = new THREE.Color().setHSL(0.66, 1, ratio); // Couleur bleue (teinte 0.66)
   const brown = new THREE.Color(0.4, 0.2, 0.1);
   const color = new THREE.Color().setHSL(brown, 1, ratio);
   colors.push(color.r, color.g, color.b);
@@ -155,16 +155,16 @@ elementMaterial.vertexColors = true;
 
 elementGeometry.colorsNeedUpdate = true; // Mettre à jour les couleurs de la géométrie
 
-// Positionner et orienter l'élément selon vos besoins
+// Positionnement et orientation des anneaux de Saturne
 elementMesh.position.set(0, 0, 0); // Les coordonnées peuvent varier selon votre scène
 elementMesh.rotation.x = Math.PI / 2; // Rotation de 90 degrés autour de l'axe X
 
-// Ajouter l'élément à votre scène
+// Ajout des anneaux à la scène
 saturnMesh.add(elementMesh);
 
 
-
-solarSystem.add(earthMesh); // Ajouter la Terre au système solaire
+// Ajouter des planètes au système solaire
+solarSystem.add(earthMesh); 
 solarSystem.add(mercuryMesh);
 solarSystem.add(venusMesh);
 solarSystem.add(marsMaterial); 
@@ -192,9 +192,9 @@ const starCount = 7000; // Nombre d'étoiles
 // Créer un tableau de positions pour les étoiles
 const positions = [];
 for (let i = 0; i < starCount; i++) {
-  const x = Math.random() * 2000 - 1000; // Coordonnée x aléatoire dans la plage -1000 à 1000
-  const y = Math.random() * 2000 - 1000; // Coordonnée y aléatoire dans la plage -1000 à 1000
-  const z = Math.random() * 2000 - 1000; // Coordonnée z aléatoire dans la plage -1000 à 1000
+  const x = Math.random() * 2000 - 1000; // Coordonnée x aléatoire dans la plage -2000 à 1000
+  const y = Math.random() * 2000 - 1000; // Coordonnée y aléatoire dans la plage -2000 à 1000
+  const z = Math.random() * 2000 - 1000; // Coordonnée z aléatoire dans la plage -2000 à 1000
   positions.push(x, y, z);
 }
 
@@ -391,7 +391,7 @@ camera.lookAt(0, 0, 0);
   
 homeButton.addEventListener('click', retourVueInitiale);
 
-// Récupérez l'élément audio
+// Récupérer l'élément audio
 const backgroundMusic = document.getElementById('background-music');
 
 // Fonction pour démarrer la musique
@@ -405,11 +405,11 @@ function stopBackgroundMusic() {
   backgroundMusic.currentTime = 0;
 }
 
-
+// Fonction pour l'animation
 function animate() {
   animationId = requestAnimationFrame(animate);
 
-    // Faire tourner les autour de leur axe Y
+    // Faire tourner les planètes autour de leur axe Y (sur elles-mêmes)
   venusMesh.rotation.y -= 0.0001;
   mercuryMesh.rotation.y -= 0.00001;
   earthMesh.rotation.y -= 0.2;
@@ -437,7 +437,7 @@ function animate() {
   neptuneMesh.position.x = 280 * Math.cos(animationTime * 0.4);
   neptuneMesh.position.z = 280 * Math.sin(animationTime * 0.4);
 
- // Rayon de l'orbite de la Lune autour de la Terre
+ 
 
 // Mettre à jour la position de la Lune pour créer l'effet d'orbite autour de la Terre
 let moonX = Math.cos(animationTime * 4) * moonOrbitRadius;
@@ -465,7 +465,7 @@ var icon = toggleButton.querySelector("i");
 // Fonction appelée lors du clic sur le bouton
 function toggleVideoDemo() {
   if (isAnimationPlaying) {
-    // Appeler la fonction d'arrêt de l'animation
+    // Appeler la fonction d'arrêt de l'animation et de la musique
     stopAnimation();
     stopBackgroundMusic();
     
@@ -476,7 +476,6 @@ function toggleVideoDemo() {
       $('.play').attr('title', "Lancer l'animation");
 
       homeButton.classList.remove('disabled');
-    //   freezeButton.classList.remove('disabled');
       ambientLightBtn.classList.remove('disabled');
   } 
 
@@ -493,7 +492,6 @@ function toggleVideoDemo() {
       
 
       homeButton.classList.add('disabled');
-    //   freezeButton.classList.add('disabled');
       ambientLightBtn.classList.add('disabled');
   }
 
@@ -577,7 +575,6 @@ function toggleAmbientLight() {
     toggleAmbientLightBtn.classList.remove("active");
     toggleAmbientLightIcon.classList.remove("fa-flip-horizontal");
 
-    // freezeButton.classList.remove('disabled');
     toggleButton.classList.remove('disabled');
   }
 
@@ -590,11 +587,11 @@ ambientLightBtn.addEventListener('click', toggleAmbientLight)
 
 
 
-const minRotationSpeed = 0.0001; // Vitesse minimale de révolution sur elle-même
-const maxRotationSpeed = 0.1; // Vitesse maximale de révolution sur elle-même
-const defaultRotationSpeed = 0.01; // Vitesse de révolution sur elle-même par défaut
-let rotationDirection = 1; // Direction de rotation sur elle-même (1 pour la rotation positive, -1 pour la rotation négative)
-let currentRotationSpeed = defaultRotationSpeed; // Vitesse de révolution sur elle-même courante
+const minRotationSpeed = 0.0001; // Vitesse minimale de révolution 
+const maxRotationSpeed = 0.1; // Vitesse maximale de révolution 
+const defaultRotationSpeed = 0.01; // Vitesse de révolution par défaut
+let rotationDirection = 1; // Direction de rotation (1 pour la rotation positive, -1 pour la rotation négative)
+let currentRotationSpeed = defaultRotationSpeed; // Vitesse de révolution courante
 
 $(document).ready(function() {
   // Créez le curseur avec les options souhaitées
@@ -604,35 +601,35 @@ $(document).ready(function() {
     step: 0.1,
     value: 0,
     slide: function(event, ui) {
-      // Mettez à jour la direction de rotation en fonction de la valeur du curseur
+      // Mettre à jour la direction de rotation en fonction de la valeur du curseur
       rotationDirection = ui.value >= 0 ? 1 : -1;
 
-      // Mettez à jour la vitesse de révolution courante avec la nouvelle vitesse absolue
+      // Mettre à jour la vitesse de révolution courante avec la nouvelle vitesse absolue
       const rotationSpeed = calculateRotationSpeed(ui.value);
       currentRotationSpeed = rotationSpeed;
     }
   });
 
-  // Démarrez l'animation par défaut
+  // Démarrer l'animation par défaut
   animateDefault();
 });
 
 function animateDefault() {
   requestAnimationFrame(animateDefault);
 
-  // Augmentez l'animationTime en fonction de la direction de rotation et de la vitesse de révolution courante
+  // Augmenter l'animationTime en fonction de la direction de rotation et de la vitesse de révolution courante
   animationTime += rotationDirection * currentRotationSpeed;
 
   // Mettez à jour la position des planètes avec l'animationTime
 }
 
 function calculateRotationSpeed(value) {
-  // Vérifiez si la valeur du curseur est égale à 0, et attribuez la vitesse de révolution par défaut dans ce cas
+  // Vérifier si la valeur du curseur est égale à 0, et attribuez la vitesse de révolution par défaut dans ce cas
   if (value === 0) {
     return 0; // La vitesse de rotation est nulle lorsque la valeur du curseur est à 0
   }
 
-  // Calculez la vitesse de révolution en fonction de la valeur du curseur
+  // Calcul de la vitesse de révolution en fonction de la valeur du curseur
   let rotationProgress = Math.abs(value);
   let rotationSpeed;
 
@@ -646,7 +643,7 @@ function calculateRotationSpeed(value) {
 }
 
 $(document).ready(function() {
-    // Ajouter l'icône de planète à l'élément de curseur
+    // Ajout de l'icône de planète à l'élément de curseur
     $('.ui-slider-handle.ui-corner-all.ui-state-default').html('<i class="fas fa-globe-americas"></i>');
     $('.ui-slider-handle.ui-corner-all.ui-state-default')
     .html('<i class="fas fa-globe-americas"></i>')
